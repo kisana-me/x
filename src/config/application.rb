f9e3c4d0e1c22daf3afb0b43f55b8b9ccca3d024
kisana <kisana@amiverse.net>
2025-08-01 15:08:46 +0900

    CHANGE: config

--- src/config/application.rb
+++ src/config/application.rb
@@ -2,28 +2,22 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    config.time_zone = 'Tokyo'
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    config.session_store :cookie_store, key: 'x_ses'
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    config.session_store :cookie_store,
+      key: '_x',
+      domain: :all,
+      tld_length: 2,
+      same_site: :lax,
+      secure: Rails.env.production?,
+      httponly: true
   end
 end
--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -1,90 +1,57 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
   config.enable_reloading = false
-
-  # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
-
-  # Full error reports are disabled.
   config.consider_all_requests_local = false
-
-  # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
-
-  # Cache assets for far-future expiry since they are all digest stamped.
+  config.assets.compile = false
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-
-  # Skip http-to-https redirect for the default health check endpoint.
-  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
-
-  # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  # config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
-
-  # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-
-  # Prevent health checks from clogging up the logs.
-  config.silence_healthcheck_path = "/up"
-
-  # Don't log any deprecations.
+  config.logger = Logger.new("log/production.log", "daily")
+  config.i18n.fallbacks = true
   config.active_support.report_deprecations = false
+  config.active_record.dump_schema_after_migration = false
+  config.active_record.attributes_for_inspect = [ :id ]
+  config.silence_healthcheck_path = "/up"
+  config.cache_store = :mem_cache_store
 
-  # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
-
-  # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
-
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [ :id ]
-
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "x.amiverse.net",     # Allow requests from example.com
-    /.*\.amiverse\.net/ # Allow requests from subdomains like `www.example.com`
+    "x.amiverse.net"
   ]
-  #
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # Cloudflare IP Ranges(https://www.cloudflare.com/ips/)
+  config.action_dispatch.trusted_proxies = [
+    # IPv6
+    IPAddr.new("2400:cb00::/32"),
+    IPAddr.new("2606:4700::/32"),
+    IPAddr.new("2803:f800::/32"),
+    IPAddr.new("2405:b500::/32"),
+    IPAddr.new("2405:8100::/32"),
+    IPAddr.new("2a06:98c0::/29"),
+    IPAddr.new("2c0f:f248::/32"),
+    # IPv4
+    IPAddr.new("103.21.244.0/22"),
+    IPAddr.new("103.22.200.0/22"),
+    IPAddr.new("103.31.4.0/22"),
+    IPAddr.new("104.16.0.0/13"),
+    IPAddr.new("104.24.0.0/14"),
+    IPAddr.new("108.162.192.0/18"),
+    IPAddr.new("131.0.72.0/22"),
+    IPAddr.new("141.101.64.0/18"),
+    IPAddr.new("162.158.0.0/15"),
+    IPAddr.new("172.64.0.0/13"),
+    IPAddr.new("173.245.48.0/20"),
+    IPAddr.new("188.114.96.0/20"),
+    IPAddr.new("190.93.240.0/20"),
+    IPAddr.new("197.234.240.0/22"),
+    IPAddr.new("198.41.128.0/17"),
+  ]
 end
